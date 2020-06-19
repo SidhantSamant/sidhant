e16a8332a609c7a40804e2915943bc3cf5a2cb36
Sidhant Samant <47266013+SidhantSamant@users.noreply.github.com>
2020-06-19 13:22:03 +0530

    updated

--- gen3/js/index.js
+++ gen3/js/index.js
@@ -1,6 +1,5 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
-const dropDown = document.querySelectorAll('.dropdown-item');
+const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -11,38 +10,3 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
-
-dropDown.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
-
-//theme
-const darkButton = document.getElementById('dark');
-const lightButton = document.getElementById('light');
-const solarButton = document.getElementById('solar');
-const body = document.body;
-
-//memory to hold theme while refresh
-const theme = localStorage.getItem('theme');
-
-if(theme){
-    body.classList.add(theme);
-}
-
-//theme button handler
-darkButton.onclick = () => {
- body.classList.replace('light','dark') ||  body.classList.replace('solar','dark');
- localStorage.setItem('theme','dark');
-};
-
-lightButton.onclick = () => {
- body.classList.replace('dark','light') ||  body.classList.replace('solar','light') ;
- localStorage.setItem('theme','light');
-};
-
-solarButton.onclick = () => {
-    body.classList.replace('light','solar') || body.classList.replace('dark','solar');
-    localStorage.setItem('theme','solar');
-};
